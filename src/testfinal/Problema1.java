a2c83e4c9a4a01f3621cb2962ed8d8a398c1d73f
gabrielastefania <gabriela stefania@DESKTOP-6R2J6ST>
2019-05-21 20:15:24 -0700

    test

---
+++ src/testfinal/Problema1.java
@@ -0,0 +1,13 @@
+package testfinal;
+
+public class Problema1 {
+	public static double harmonicAvg(int a, int b) {
+		double md = (2*a*b)/(a+b);
+		if (a + b == 0) {
+			return 0;
+		
+	}
+		return md;
+		
+	} 
+}
